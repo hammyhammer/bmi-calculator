3b3b9f84841e36fdbe34fc414989c04432e5dd48
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-01-11 09:20:13 -0500

    created functions for ImperialSystem

--- client/src/components/ImperialSystem.jsx
+++ client/src/components/ImperialSystem.jsx
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+import api from '../services/apiConfig'
 
 export default function ImperialSystem() {
+  const default_input = {
+    weight: 0,
+    height: 0,
+  }
+
+  const [input, setInput] = useState({});
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const fields = input;
+    const res = await api.post("", { fields })
+    console.log(res.data)
+    setInput(default_input)
+  }
+
+  const handleMetricInput = (event) => {
+    const { id, valueAsNumber } = event.target;
+    setInput(prevInput => ({
+      ...prevInput,
+      [id]: valueAsNumber,
+    }));
+  };
   return (
     <div>
+
 
     </div>
   )
