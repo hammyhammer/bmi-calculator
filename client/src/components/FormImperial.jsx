d67f4e2ca16d2ea91ca5164ca895e16423043386
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-01-11 11:09:10 -0500

    adding props

--- client/src/components/FormImperial.jsx
+++ client/src/components/FormImperial.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 export default function FormImperial() {
+  const { weight, height } = props.input
+
   return (
     <div>
-      <form>
-        <input id="weight" value={weight} type="number" onChange={handleImperialInput} />
+      <form onSubmit={props.handleSubmit}>
+        <label>weight</label>
+        <input id="weight" value={weight} type="number" placeholder="inches" onChange={props.handleImperialInput} />
         <br />
-        <input id="height" value={height} type="number" onChange={handleImperialInput} />
+        <input id="height" value={height} type="number" onChange={props.handleImperialInput} />
       </form>
     </div>
   )
