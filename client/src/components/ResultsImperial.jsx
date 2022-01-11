d55e595a02176b90c206beaf19ec76a008229d7c
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-01-11 16:28:43 -0500

    trying to use conditionals

--- client/src/components/ResultsImperial.jsx
+++ client/src/components/ResultsImperial.jsx
@@ -14,12 +14,31 @@ export default function ResultsImperial() {
     fetchSum();
   }, []);
 
+  let risks = "";
+  let riskCategory = sum.fields?.calulationImperial;
+
+  if (riskCategory < 18.5) {
+    risks = 'Risks associated with the underweight classification include weakened immune function, malnourishment, and osteoporosis.'
+  }
+  else if (riskCategory >= 18.5 && riskCategory <= 24.9) {
+    risks = 'normal'
+  }
+  else if (riskCategory >= 25.0 && riskCategory <= 29.9) {
+    risks = "risks of being overwieght"
+  }
+  else if (riskCategory >= 30.0) {
+    risks = "risks fo being obese"
+  }
+  console.log(risks);
 
   return (
     <div>
       <h1>Your Results</h1>
       <h5>Your BMI is calculated to {Number.parseFloat(sum.fields?.calculationImperial).toFixed(2)}%. <br />
         With a BMI of {Number.parseFloat(sum.fields?.calculationImperial).toFixed(2)}%, you are placed in the {sum.fields?.categoryImperial} category.</h5>
+      <p>BMI is used as a screening tool. BMI is not intended to diagnose any illnesses<a href="https://www.cdc.gov/healthyweight/assessing/bmi/index.html">[1]</a>.</p>
+      <br />
+      <p>{risks}</p>
       <Link to={`/imperial-results/${id}`}></Link>
     </div>
   )
--- client/src/components/ResultsMetric.jsx
+++ client/src/components/ResultsMetric.jsx
@@ -20,6 +20,8 @@ export default function ResultsPage() {
       <h5>Your BMI is calculated to {Number.parseFloat(sum.fields?.calculationMetric).toFixed(2)}%. <br />
         With a BMI of {Number.parseFloat(sum.fields?.calculationMetric).toFixed(2)}%, you are placed in the {sum.fields?.categoryMetric} category.</h5>
       <p>BMI is used as a screening tool. BMI is not intended to diagnose any illnesses<a href="https://www.cdc.gov/healthyweight/assessing/bmi/index.html">[1]</a>.</p>
+      <br />
+      <p>{sum.fields?.riskCategory}</p>
       <Link to={`/metric-results/${id}`}></Link>
     </div>
   )
