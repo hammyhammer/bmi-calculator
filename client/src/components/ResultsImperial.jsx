cad38048ab80af65cd2428ab9c9b4ae1429fae7e
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-01-11 12:23:37 -0500

    progress in resultsimperial

--- client/src/components/ResultsImperial.jsx
+++ client/src/components/ResultsImperial.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import api from "../services/apiConfig"
 
 export default function ResultsImperial() {
+  const [sum, setSum] = useState({})
+  const { id } = useParams
+
+  useEffect(() => {
+    const fetchSum = async () => {
+      const res = await api.get(`/${id}`)
+      setSum(res.data)
+    }
+    fetchSum();
+  }, []);
+
+
   return (
     <div>
       THIS IS THE ***IMPERIAL** RESULTS PAGE
+      <h3>BMI: {sum.fields?.calculationImperial}%</h3>
+      <h4>Category: {sum.fields?.categoryImperial}</h4>
+      <Link to={`/imperial-results/${id}`}></Link>
     </div>
   )
 }
