7a71dcf471ed7da97d3cf781ba03ee76704489ff
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-01-12 15:16:45 -0500

    styling the main page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,11 +4,13 @@ export default function Home() {
   return (
     <div>
       <h2>Adult BMI Assessment</h2>
-      <p style={{ padding: "10px 300px 0px 300px" }}>BMI (Body Mass Index) is a quick calculation in which we take your weight in kilograms and divide
-        by your height squared (kg/m^2). It is safe, and free assessment that could provided insight to any potential
-        health-related issues.</p>
-      <div className="horizontal"></div>
-      <p>If you are interested to see what your BMI is, please select the system of measurement you are most familiar with.</p>
+      <div className="main-body">
+        <p style={{ padding: "10px 300px 0px 300px" }}>BMI (Body Mass Index) is a quick calculation in which we take your weight in kilograms and divide
+          by your height squared (kg/m^2). It is safe, and free assessment that could provided insight to any potential
+          health-related issues.</p>
+        <div className="horizontal"></div>
+        <p>If you are interested to see what your BMI is, please select the system of measurement you are most familiar with.</p>
+      </div>
       <Link to="/imperial">
         <button>Imperial</button>
       </Link>
