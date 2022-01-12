17d6e44ee3df5e46f6132b1cf77b63c2e70d350a
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-01-12 14:29:31 -0500

    header complete, no link or git

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,27 +3,30 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <header>
-      <Link to="/">
-        Home
+      <Link
+        className="home"
+        to="/">
+        <button>Home</button>
       </Link>
 
-      <Link to="/why-bmi">
-        Why BMI?
+      <Link
+        className="why-bmi"
+        to="/why-bmi">
+        <button>Why BMI?</button>
       </Link>
-      <div className="socials">
-        <a href="https://github.com/hammyhammer">
-          <img
-            className="git"
-            src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png"
-            alt="Github Icon" />
-        </a>
-        <a href="https://www.linkedin.com/in/hampton-jacobs-a80821126/">
-          <img
-            className="link"
-            src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/linkedin_vnvo6s.png"
-            alt="Linkedln Icon" />
-        </a>
-      </div>
+
+      {/* <a href="https://github.com/hammyhammer">
+        <img
+          className="git"
+          src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png"
+          alt="Github Icon" />
+      </a>
+      <a href="https://www.linkedin.com/in/hampton-jacobs-a80821126/">
+        <img
+          className="link"
+          src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/linkedin_vnvo6s.png"
+          alt="Linkedln Icon" />
+      </a> */}
     </header>
   )
 }
