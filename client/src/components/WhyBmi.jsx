3170b3dd42058ca1ec11dfc12cead4060d31e30c
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-01-13 16:40:06 -0500

    working on why bmi

--- client/src/components/WhyBmi.jsx
+++ client/src/components/WhyBmi.jsx
@@ -2,13 +2,20 @@ export default function WhyBmi() {
   return (
     <div>
       <h2>Why BMI?</h2>
-      <p>BMI is a simple and easy to do calculation that allows insight to any potneital health issues.
-        It is important to note that BMI is a not a diagnosis, nor it is an indicator of one's full body composition.
-      </p>
-      <div>
+      <div className="why-top">
+        <p>BMI is a simple and easy to do calculation that allows insight to any potential health issues.
+          It is important to note that BMI is a not a diagnosis of any illness[1].
+          <br />
+          However, BMI is correlated with chronic diseases that are closely related body composition<a href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html">[2]</a>.
+        </p>
+      </div>
+
+      <div className="met-results-horizontal"></div>
+
+      <div className="why-bottom">
+        <p>Examples of chronic diseases include such as hypertension, type II diabetes, heart diease, breathing problems, and certain cancers<a href="https://www.nhlbi.nih.gov/health/educational/lose_wt/risk.htm">[3]</a>.</p>
 
       </div>
-      <p>However, BMI can coorelate with metabolic diseases, such as hypertension, diabetes, heart diease, etc</p>
     </div>
   )
 }
