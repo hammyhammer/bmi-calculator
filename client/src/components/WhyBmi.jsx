ce8e28aecc0628a3f8cb122685b0f2fdbc4963e3
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-01-13 17:22:43 -0500

    why bmi completed

--- client/src/components/WhyBmi.jsx
+++ client/src/components/WhyBmi.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export default function WhyBmi() {
   return (
     <div>
       <h2>Why BMI?</h2>
       <div className="why-top">
         <p>BMI is a simple and easy to do calculation that allows insight to any potential health issues.
-          It is important to note that BMI is a not a diagnosis of any illness[1].
+          It is important to note that BMI is a not a diagnosis of any illness<a href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Used">[1]</a>.
+          <br />
           <br />
           However, BMI is correlated with chronic diseases that are closely related body composition<a href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html">[2]</a>.
         </p>
@@ -12,8 +15,20 @@ export default function WhyBmi() {
 
       <div className="met-results-horizontal"></div>
 
-      <div className="why-bottom">
-        <p>Examples of chronic diseases include such as hypertension, type II diabetes, heart diease, breathing problems, and certain cancers<a href="https://www.nhlbi.nih.gov/health/educational/lose_wt/risk.htm">[3]</a>.</p>
+      <div className="why-middle">
+        <p>Some examples of chronic diseases include such as hypertension, type II diabetes, heart diease, and certain cancers<a href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Consequences">[3]</a>.</p>
+
+        <div className="met-results-horizontal"></div>
+
+        <div className="why-bottom">
+          <p>BMI has 4 categories: <br />
+            Underweight: less than 18.5. <br />
+            Normal: 18.5 to less than 25. <br />
+            Overweight: 25 to less than 30. <br />
+            Obese: 30.0 or higher<a href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Used">[4]</a>.</p>
+
+        </div>
+
 
       </div>
     </div>
