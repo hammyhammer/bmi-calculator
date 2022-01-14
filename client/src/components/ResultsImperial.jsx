510f6c0c5979d67f67dd598e53b6e87fda1645dd
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-01-14 15:06:52 -0500

    conditionals

--- client/src/components/ResultsImperial.jsx
+++ client/src/components/ResultsImperial.jsx
@@ -5,30 +5,33 @@ import api from "../services/apiConfig"
 export default function ResultsImperial() {
   const [sum, setSum] = useState({})
   const { id } = useParams();
+  let risks = "";
 
   useEffect(() => {
     const fetchSum = async () => {
       const res = await api.get(`/imperial/${id}`)
       setSum(res.data)
+
+
+      let riskCategory = sum.fields?.calulationImperial;
+
+      if (riskCategory < 18.5) {
+        risks = 'Risks associated with the underweight classification include weakened immune function, malnourishment, and osteoporosis.'
+      }
+      else if (riskCategory >= 18.5 && riskCategory <= 24.9) {
+        risks = 'normal'
+      }
+      else if (riskCategory >= 25.0 && riskCategory <= 29.9) {
+        risks = "risks of being overwieght"
+      }
+      else if (riskCategory >= 30.0) {
+        risks = "risks fo being obese"
+      }
     }
     fetchSum();
   }, []);
 
-  // let risks = "";
-  // let riskCategory = sum.fields?.calulationImperial;
 
-  // if (riskCategory < 18.5) {
-  //   risks = 'Risks associated with the underweight classification include weakened immune function, malnourishment, and osteoporosis.'
-  // }
-  // else if (riskCategory >= 18.5 && riskCategory <= 24.9) {
-  //   risks = 'normal'
-  // }
-  // else if (riskCategory >= 25.0 && riskCategory <= 29.9) {
-  //   risks = "risks of being overwieght"
-  // }
-  // else if (riskCategory >= 30.0) {
-  //   risks = "risks fo being obese"
-  // }
 
   return (
     <div>
@@ -48,6 +51,10 @@ export default function ResultsImperial() {
           <p>BMI is used as a screening tool. BMI is not intended to diagnose any illnesses
             <a href="https://www.cdc.gov/healthyweight/assessing/bmi/index.html">[1]</a>.</p>
           <p>{sum.fields?.riskCategory}</p>
+          {sum.fields.categoryImperial === 'Underweight' ? <p>Citation for underweight</p> :
+            sum.fields.categoryImperial === "Normal" ? <p>Citation for Normal</p> :
+              sum.fields.categoryImperial === "Overweight" ? <p>Citation for Overweight</p> :
+                <p>Citation for obese</p>}
           <p>To gain more information or pursue action. Reach out to your healthcare provider for additional information.</p>
         </div>
         <Link to={`/metric-results/${id}`}></Link>
