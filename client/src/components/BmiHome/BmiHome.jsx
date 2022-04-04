c9ed8d7ab8a2e81f96ebd185916ff5b3399ca052
Hampton Jacobs <hammyjacobs@gmail.com>
2022-04-04 12:08:49 -0400

    bmi homepage refactored for mobile. Styling

--- client/src/components/BmiHome/BmiHome.jsx
+++ client/src/components/BmiHome/BmiHome.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import bmihomecss from './BmiHome.module.css';
 
 export default function Home() {
   return (
-    <div className='bmi-home'>
-      <h2 className="header">Adult BMI Assessment</h2>
+    <div className={bmihomecss.bmi_home}>
+      <h2 className={bmihomecss.header}>Adult BMI Assessment</h2>
       <div className="horizontal"></div>
-      <div className="home-main-body">
+      <div className={bmihomecss.main_body}>
         <p>BMI (Body Mass Index) is a measurement between the ratio of your height and weight.
           It is a good way to gauge your body's composition.
         </p>
@@ -14,16 +15,16 @@ export default function Home() {
       </div>
 
       <div className="horizontal"></div>
-      <div className="bottom">
+      <div className={bmihomecss.bottom}>
         <p>Please select the system of measurement you are most familiar with.</p>
         <Link to="/imperial">
-          <button className="imperial">Imperial</button>
+          <button className={bmihomecss.imperial}>Imperial</button>
         </Link>
         <p>(Inches, pounds.)</p>
 
         <br />
         <Link to="/metric">
-          <button className="metric">Metric</button>
+          <button className={bmihomecss.metric}>Metric</button>
         </Link>
         <p>(Meters, kilograms.)</p>
 
