d74ec0c0c068ff0c820305ec15f795f26e04fb1b
Hampton Jacobs <hammyjacobs@gmail.com>
2022-04-04 12:45:56 -0400

    bmi resource page and imp results styled  refactored

--- client/src/ResultsImperial/ResultsImperial.jsx
+++ client/src/ResultsImperial/ResultsImperial.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import api from "../services/apiConfig";
+import resultsimperialcss from './ResultsImperial.module.css'
 
 export default function ResultsImperial() {
   const [sum, setSum] = useState({});
@@ -20,7 +21,7 @@ export default function ResultsImperial() {
   return (
     <div>
       <div>
-        <h2 className="header">Your BMI Results</h2>
+        <h2 className={resultsimperialcss.header}>Your BMI Results</h2>
         <div className="horizontal"></div>
         <br />
         <h3>Your BMI is calculated to {Number.parseFloat(sum.fields?.calculationImperial).toFixed(2)}%.
@@ -31,20 +32,20 @@ export default function ResultsImperial() {
       </div>
       <div>
         <div className="horizontal"></div>
-        <div className="body">
+        <div className={resultsimperialcss.body}>
           <p>BMI is used as a screening tool. BMI is not intended to diagnose any illnesses
-            <a className="reference" href="https://www.cdc.gov/healthyweight/assessing/bmi/index.html">[1]</a>.</p>
+            <a className={resultsimperialcss.references} href="https://www.cdc.gov/healthyweight/assessing/bmi/index.html">[1]</a>.</p>
           <p>{sum.fields?.riskCategory}
-            {sum.fields?.categoryImperial === 'Underweight' ? <a className="reference" href="https://www.healthline.com/health/underweight-health-risks#malnutrition">[2].</a> :
-              sum.fields?.categoryImperial === "Normal" ? <a className="reference" href="https://www.aicr.org/resources/blog/a-normal-bmi-doesnt-mean-youre-healthy/">[2].</a> :
-                sum.fields?.categoryImperial === "Overweight" ? <a className="reference" href="https://www.betterhealth.vic.gov.au/health/healthyliving/body-mass-index-bmi#risks-of-being-overweight-high-bmi-and-physically-inactive">[2].</a> :
-                  <a className="reference" href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Consequences">[2].</a>}</p>
+            {sum.fields?.categoryImperial === 'Underweight' ? <a className={resultsimperialcss.references} href="https://www.healthline.com/health/underweight-health-risks#malnutrition">[2].</a> :
+              sum.fields?.categoryImperial === "Normal" ? <a className={resultsimperialcss.references} href="https://www.aicr.org/resources/blog/a-normal-bmi-doesnt-mean-youre-healthy/">[2].</a> :
+                sum.fields?.categoryImperial === "Overweight" ? <a className={resultsimperialcss.references} href="https://www.betterhealth.vic.gov.au/health/healthyliving/body-mass-index-bmi#risks-of-being-overweight-high-bmi-and-physically-inactive">[2].</a> :
+                  <a className={resultsimperialcss.references} href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Consequences">[2].</a>}</p>
           <p>To gain more information or pursue action. Reach out to your healthcare provider for additional information.</p>
 
 
           <Link
             to="/why-bmi">
-            <button className="info-button">Click here to learn more about BMI</button>
+            <button className={resultsimperialcss.info_button}>Click here to learn more about BMI</button>
           </Link>
         </div>
       </div>
--- client/src/WhyBmi/WhyBmi.jsx
+++ client/src/WhyBmi/WhyBmi.jsx
@@ -1,33 +1,34 @@
 import { Link } from 'react-router-dom';
+import whybmicss from './WhyBmi.module.css';
 
 export default function WhyBmi() {
   return (
-    <div classname="why">
-      <h2 className="header">Why BMI?</h2>
+    <div classname={whybmicss.why}>
+      <h2 className={whybmicss.header}>Why BMI?</h2>
       <div className="horizontal"></div>
       <br />
-      <div className="why-top">
-        <p>It is important to note that BMI is a not a diagnosis of any illness<a className="reference" href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Used">[1]</a>.
+      <div className={whybmicss.body}>
+        <p>It is important to note that BMI is a not a diagnosis of any illness<a className={whybmicss.reference} href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Used">[1]</a>.
           <br />
-          However, BMI is correlated with chronic diseases that are closely related body composition<a className="reference" href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html">[2]</a>.
+          However, BMI is correlated with chronic diseases that are closely related body composition<a className={whybmicss.reference} href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html">[2]</a>.
         </p>
       </div>
-      <div className="why-middle">
-        <p>Some examples of chronic diseases include such as hypertension, type II diabetes, heart diease, and certain cancers<a className="reference" href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Consequences">[3]</a>.</p>
+      <div className={whybmicss.body}>
+        <p>Some examples of chronic diseases include such as hypertension, type II diabetes, heart diease, and certain cancers<a className={whybmicss.reference} href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Consequences">[3]</a>.</p>
       </div>
       <div className="horizontal"></div>
 
-      <div className="why-bottom">
+      <div className={whybmicss.body}>
         <p>BMI has 4 categories: <br />
           Underweight: Less than 18.5%. <br />
           Normal: 18.5% to less than 25%. <br />
           Overweight: 25% to less than 30%. <br />
-          Obese: 30.0% or higher<a className="reference" href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#InterpretedAdults">[4]</a>.</p>
+          Obese: 30.0% or higher<a className={whybmicss.reference} href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#InterpretedAdults">[4]</a>.</p>
 
       </div>
       <Link
         to="/">
-        <button className="info-button">Return Home</button>
+        <button className={whybmicss.info_button}>Return Home</button>
       </Link>
 
 
