7327ff64917e6e1aa00f7bcda6180d6e9cd93787
Hampton Jacobs <hammyjacobs@gmail.com>
2022-04-08 12:35:36 -0400

    doing desktop queries for bmi home and home page

--- client/src/components/FormMetric/FormMetric.jsx
+++ client/src/components/FormMetric/FormMetric.jsx
@@ -7,12 +7,12 @@ export default function Form(props) {
   return <form onSubmit={props.handleSubmit}>
     <br />
     <input className={metricformcss.met_weight} id="weightMetric" value={weightMetric} type="number" placeholder="kilograms" onChange={props.handleMetricInput} />
-    <label className="icon-weight-met">
+    <label className={metricformcss.icon_weight}>
       <i className="fas fa-weight fa-3x"></i>
     </label>    <br />
     <br />
     <input className={metricformcss.met_height} id="heightMetric" value={heightMetric} type="number" placeholder="centimeters" onChange={props.handleMetricInput} />
-    <label className="icon-height-met">
+    <label className={metricformcss.icon_height}>
       <i className="fas fa-ruler-vertical fa-3x"></i>
     </label>
     <br />
