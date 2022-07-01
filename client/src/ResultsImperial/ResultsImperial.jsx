62d3fcb1cc08d8c8da495bb551d5841f477b3b29
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-01 14:01:58 -0400

    added target to links for results Imperial

--- client/src/ResultsImperial/ResultsImperial.jsx
+++ client/src/ResultsImperial/ResultsImperial.jsx
@@ -34,12 +34,12 @@ export default function ResultsImperial() {
         <div className="horizontal"></div>
         <div className={resultsimperialcss.body}>
           <p>BMI is used as a screening tool. BMI is not intended to diagnose any illnesses
-            <a className={resultsimperialcss.references} href="https://www.cdc.gov/healthyweight/assessing/bmi/index.html">[1]</a>.</p>
+            <a className={resultsimperialcss.references} href="https://www.cdc.gov/healthyweight/assessing/bmi/index.html" target="_blank" rel="noopener noreferrer">[1]</a>.</p>
           <p>{sum.fields?.riskCategory}
-            {sum.fields?.categoryImperial === 'Underweight' ? <a className={resultsimperialcss.references} href="https://www.healthline.com/health/underweight-health-risks#malnutrition">[2].</a> :
-              sum.fields?.categoryImperial === "Normal" ? <a className={resultsimperialcss.references} href="https://www.aicr.org/resources/blog/a-normal-bmi-doesnt-mean-youre-healthy/">[2].</a> :
-                sum.fields?.categoryImperial === "Overweight" ? <a className={resultsimperialcss.references} href="https://www.betterhealth.vic.gov.au/health/healthyliving/body-mass-index-bmi#risks-of-being-overweight-high-bmi-and-physically-inactive">[2].</a> :
-                  <a className={resultsimperialcss.references} href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Consequences">[2].</a>}</p>
+            {sum.fields?.categoryImperial === 'Underweight' ? <a className={resultsimperialcss.references} href="https://www.healthline.com/health/underweight-health-risks#malnutrition" target="_blank" rel="noopener noreferrer">[2].</a> :
+              sum.fields?.categoryImperial === "Normal" ? <a className={resultsimperialcss.references} href="https://www.aicr.org/resources/blog/a-normal-bmi-doesnt-mean-youre-healthy/" target="_blank" rel="noopener noreferrer">[2].</a> :
+                sum.fields?.categoryImperial === "Overweight" ? <a className={resultsimperialcss.references} href="https://www.betterhealth.vic.gov.au/health/healthyliving/body-mass-index-bmi#risks-of-being-overweight-high-bmi-and-physically-inactive" target="_blank" rel="noopener noreferrer">[2].</a> :
+                  <a className={resultsimperialcss.references} href="https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Consequences" target="_blank" rel="noopener noreferrer">[2].</a>}</p>
           <p>To gain more information or pursue action. Reach out to your healthcare provider for additional information.</p>
 
 
